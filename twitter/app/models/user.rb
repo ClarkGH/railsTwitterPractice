a3a512b96d6a213810b3b698d1953276ff282485
clarkgh <demolisho@hotmail.com>
2018-07-02 00:14:36 -0700

    relationships added

--- twitter/app/models/user.rb
+++ twitter/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :followers, :class_name => "Relationships", :foreign_key => "user_id"
+  has_many :following, :class_name => "Relationships", :foreign_key => "follower_id"
+  has_many :likes
+  has_many :tweets
 end
